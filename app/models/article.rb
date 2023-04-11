5647f36b9946323b9b3f5951606b689339f5d4c7
Baldemar López del Carpio Juárez <baldemar_del_carpio@outlook.com>
2023-04-10 23:14:03 -0600

    Add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+
+	validates :title, presence: true, length: {minimun: 6, maximun: 100 }
+	validates :description, presence: true, length: {minimun: 10, maximun: 3000}
+
 end
