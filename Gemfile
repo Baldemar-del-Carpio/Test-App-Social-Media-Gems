780edd7d60794294ec28eda3f278d8ebf23a3872
Baldemar López del Carpio Juárez <54747000+Baldemar-del-Carpio@users.noreply.github.com>
2023-05-03 13:51:06 -0600

    Add gems

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Megaphone API
+gem 'megaphone-client'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 
-	validates :title, presence: true, length: {minimun: 6, maximun: 100 }
-	validates :description, presence: true, length: {minimun: 10, maximun: 3000}
+	validates :title, presence: true, length: {minimum: 6, maximum: 100 }
+	validates :description, presence: true, length: {minimum: 10, maximum: 3000}
 
 end
